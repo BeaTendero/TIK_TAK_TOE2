5d89f2b345c1119ed5775ae1233c9937968abcb8
beatendero <beatriz.tendero.hernandez@gmail.com>
2022-10-10 09:41:38 +0200

    proyecto2base

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+var player1; //True o False
+var nuevoJuego = false; // Primer Juego
+
+const sortearPartida = document.querySelector("#ranGame")
+
+sortearPartida.onclick = () => {
+player1 = random(nuevoJuego);
+
+}
